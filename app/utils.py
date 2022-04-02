35e003e82811bfaaf1893a784271ff26856e9e02
stacyche <97638440+stacyche@users.noreply.github.com>
2022-04-02 18:18:05 -0400

    Simpler (#4)
    * Setup package requirements
    
    * Refactor across files
    
    * init file change

---
+++ app/utils.py
@@ -0,0 +1,15 @@
+
+
+def to_usd(my_price):
+    '''
+    This is a docstring. It tells us what this function is about. 
+    What its responsibilities are. 
+    What the parama are about 
+    What datatypes the params are. 
+    What this function will return. 
+    Example of invoking the function 
+
+    Invoke like this: to_usd(9.999)
+    '''
+    return '${:,.2f}'.format(my_price)
+
